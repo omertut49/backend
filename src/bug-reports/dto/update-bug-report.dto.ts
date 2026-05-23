045f30ed62b782730d95d1aef793498217915eb2
Ömer Tut <tutomer86@gmail.com>
2026-05-23 16:18:44 +0300

    reset

--- src/bug-reports/dto/update-bug-report.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBugReportDto } from './create-bug-report.dto';
-
-export class UpdateBugReportDto extends PartialType(CreateBugReportDto) {}
